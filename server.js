740a93ca3978e573f73b2cb303920be7ba77fbaf
yaseen waleed <yaseen.dev15@gmail.com>
2025-08-24 15:58:29 +0300

    شرح التعديلات اللي عملتها

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose';
 import Stripe from 'stripe';
-import userRoutes from '../backend/routes/userRoues.js'
+import userRoutes from './routes/userRoutes.js'
 import productRoutes from '../backend/routes/productRoutes.js'
 import adminRoutes from '../backend/routes/adminRoutes.js'
 dotenv.config()
